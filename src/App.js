116d88b1c7026c8a259ef0723f7bc304979903a4
Kevin <kevinbluth5@gmail.com>
2020-03-06 10:30:37 -0700

    updatingproject

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import Person from "./person/person"
-import StatefulComponent from "./state/StatefulComponent";
 
 function App() {
   // const array1= [5,3,22,55]
   return (
     <div className="App">
       <Person firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
-      <Person firstName="Smith" lastName="John" age={88} hairColor="Brown" />
+      <Person firstName="John" lastName="Smith" age={88} hairColor="Brown" />
+      <Person firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown" />
+      <Person firstName="Maria" lastName="Smith" age={62} hairColor="Brown" />
     </div>
   );
 }
