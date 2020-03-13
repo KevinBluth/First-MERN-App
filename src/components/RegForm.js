880de6722f5f5cd5782628f8243819cd5cc8959e
Kevin <kevinbluth5@gmail.com>
2020-03-13 11:37:14 -0600

    more forms

--- src/components/RegForm.js
+++ src/components/RegForm.js
@@ -7,16 +7,22 @@ const RegForm = props => {
         lName: "",
         email: "",
         password: "",
-        confirmPW: ""
+        confirmPW: "",
+        submitted: false
     })
     const onChangeHandler = event => {
         setFormState({
             ...formState,
             [event.target.name]: event.target.value
-        });;
+        });
+        
     }
     const onSubmitHandler = event => {
-        event.prevent.Default();
+        event.preventDefault();
+        setFormState({
+            ...formState,
+            [event.target.submitted]: true
+        });
         console.log(formState);
     }
     return(
@@ -24,18 +30,23 @@ const RegForm = props => {
             <form onSubmit={onSubmitHandler}>
                 <label>First Name</label>
                 <input type="text" name="fName" onChange={onChangeHandler}/>
+                {formState.fName.length > 0 && formState.fName.length < 2 && (<span>Not long enough</span>)}
                 <br/>        
                 <label>Last Name</label>
                 <input type="text" name="lName" onChange={onChangeHandler}/>
+                {formState.lName.length > 0 && formState.lName.length < 2 && (<span>Not long enough</span>)}
                 <br/>        
                 <label>Email</label>
                 <input type="email" name="email" onChange={onChangeHandler}/>
+                {formState.email.length > 0 && formState.email.length < 5 && (<span>Email needs to be at least</span>)}
                 <br/>        
                 <label>Password</label>
                 <input type="password" name="password" onChange={onChangeHandler}/>
+                {formState.password.length > 0 && formState.password.length < 8 && (<span>Password needs to be 8 characters</span>)}
                 <br/>        
                 <label>Confirm Password</label>
                 <input type="password" name="confirmPW" onChange={onChangeHandler}/>
+                {formState.confirmPW.match(formState.password) && formState.confirmPW < 8 && (<span>Passwords need to match</span>)}
                 <br/>   
                 <input type="submit" />     
             </form>
